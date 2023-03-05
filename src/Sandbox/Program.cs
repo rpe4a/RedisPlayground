a5ec3f5b0a59334735e61dbb72f829fc582fb472
Mikhail Plyusnin <m.plyusnin@cloudaware.com>
2023-03-05 22:49:23 +0400

    initialize project

---
+++ src/Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
